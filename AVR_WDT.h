273a21646956f9e3942a2deabc6857d427a2ff81
Ankit Bhatnagar <ankit.bhatnagarindia@gmail.com>
2016-12-22 11:34:13 +0530

    added basic library files structure

---
+++ AVR_WDT.h
@@ -0,0 +1,17 @@
+//////////////////////////////////////////////
+// AVR WATCH DOG TIMER LIBRARY
+//
+//
+// DECEMBER 22, 2016
+//
+// ANKIT BHATNAGAR
+// ANKIT.BHATNAGARINDIA@GMAIL.COM
+//////////////////////////////////////////////
+
+#ifndef _AVR_WDT_H_
+#define _AVR_WDT_H_
+
+#include <stdio.h>
+#include <avr/io.h>
+
+#endif
